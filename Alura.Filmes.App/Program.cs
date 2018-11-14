69dd04c59cd61a45338df1bb3ca50a24c9da91ee
Guilherme <guilherme.golfetto@hotmail.com>
2018-11-14 00:38:31 -0200

    primeira aula

---
+++ Alura.Filmes.App/Program.cs
@@ -0,0 +1,20 @@
+﻿using Alura.Filmes.App.Dados;
+
+namespace Alura.Filmes.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var contexto = new AluraFilmesContexto())
+            {
+                foreach (var item in contexto.Atores)
+                {
+                    System.Console.WriteLine(item);
+
+                }
+                System.Console.ReadKey();
+            }
+        }
+    }
+}
